0c1f06f3cdbe451d750c8f7f472bd74f4a9f769a
thanhbinh2304 <binh2304tto@gmail.com>
2025-12-17 06:07:06 +0700

    fix cors

--- BE-Travely/config/cors.php
+++ BE-Travely/config/cors.php
@@ -2,24 +2,14 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://fe-travely.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +19,7 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
+
